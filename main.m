6f646c8c64e43e72d42724521535d43e529f97e2
Charles Roux <charles-andre.roux@mail.mcgill.ca>
2022-10-06 12:11:47 -0400

    update

--- main.m
+++ main.m
@@ -1,7 +1,9 @@
 
 
-function xva = main(N, Tl, Tr, lambda, k, tstep, deltaM, maxStep, beta)
+function [dat,j,time] = main(N, Tl, Tr, lambda, k, tstep, deltaM, maxStep, beta)
+
 t = tic();
+
 if ~exist('N', 'var')
     N = 1024;
 end
@@ -24,7 +26,7 @@ if ~exist('deltaM', 'var')
     deltaM = 0;
 end
 if ~exist('maxStep', 'var')
-    maxStep = 10e5;
+    maxStep = 1e7;
 end
 if ~exist('beta', 'var')
     beta = 1;
@@ -38,48 +40,78 @@ v = zeros(N + 2, 1);
 a = zeros(N + 2, 1);
 v(2) = Tl;
 v(N+1) = Tr;
+xil0 = 2.*lambda.*kB.*Tl;
+xir0 = 2.*lambda.*kB.*Tr;
 
 %array of masses
-m = zeros(N+2, 1);
 m = 1 - deltaM + (2*deltaM)*rand(N + 2, 1);
 m(1) = inf;
 m(N+2) = inf;
 
-xiL = sqrt(2*lambda*kB*Tl);
-xiR = sqrt(2*lambda*kB*Tr);
+newa = zeros(N+2,1);
+xl = zeros(N+2,1);
+xr = zeros(N+2,1);
+fl = zeros(N+2,1);
+fstep = zeros(N+2,1); 
+cubel = zeros(N+2,1);
+cuber = zeros(N+2,1);
 
+fcum = zeros(N+2, 1);
 
-for step = 1:maxStep
-    
-    newx = zeros(N+2, 1);
-    newx = x + v.*tstep + 0.5.*a.*tstep.^2;
-    newa = zeros(N+2, 1);
+for step = 1:1000
 
-    for i = 2:N+1
-        
-        % calculating a(t + delta_t)
-        newa(i) = k*(newx(i-1) - 2*newx(i) + newx(i+1))...
-            + beta.*((newx(i-1) - newx(i))^3 + (newx(i+1) - newx(i)).^3);
-        
-    end
+    x = x + v.*tstep + 0.5.*a.*tstep.^2;
 
-    newa(2) = newa(2) + xiL - lambda.*v(2);
-    newa(N+1) = newa(N+1) + xiR - lambda.*v(N+1);
-    newa = newa./m;
+    xl = circshift(x, 1);
+    xl(N+2) = 0;
+
+    cubel = (xl - x);
+    cubel = cubel.*cubel.*cubel;
+    cuber = -circshift(cubel,-1);
+    cuber(N+1) = (x(N+1) - x(N))^3;
+    cuber(1) = 0;
+
+    xr = circshift(x,-1);
+    xr(1) = 0;
+
+%     x = gpuArray(x);
+%     xl = gpuArray(xl);
+%     xr = gpuArray(xr);
+%     fl = gpuArray(fl);
+%     cubel = gpuArray(cubel);
+%     cuber = gpuArray(cuber);
+%     v = gpuArray(v);
+ 
+    fl = k.*xl + beta.*cubel;
+    fstep = fl + k.*(- 2.*x + xr) + beta.*cuber;
+
+    xiL = xil0*randn;
+    xiR = xir0*randn;
+
+    fstep(2) = fstep(2) + xiL - lambda.*v(2);
+    fstep(N+1) = fstep(N+1) + xiR - lambda.*v(N+1);
+
+    newa = fstep./m;
 
 
     % calculating v(t + delta_t)
-    newv = zeros(N+2, 1);
-    newv = v + 0.5.*(a + newa).*tstep;
-    
-    x = newx;
-    v = newv;
+    v = v + 0.5.*(a + newa).*tstep;
+
     a = newa;
+    fcum = fcum + fl.*v;
+
+    
     
 end
 
-xva = [x,v,a];
-toc(t);
+%disp(fstep);
+%disp(fcum);
+%disp(j);
+%disp(sum(fl)/N);
+
+j = sum(fcum)/(maxStep.*(N-1));
+time = toc(t);
+dat = [x,v,a];
 
 end
 
