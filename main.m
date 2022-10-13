d306ba72cc993fcf25bb6e303f52522a9da2d67a
Charles Roux <charles-andre.roux@mail.mcgill.ca>
2022-10-13 11:05:19 -0400

    update

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 
 
-function [dat,j,time] = main(N, Tl, Tr, lambda, k, tstep, deltaM, maxStep, beta)
+function [dat,j,time] = main(N, Tl, Tr, lambda, k, tstep, deltaM, maxStep, beta, nDataPoints, save)
 
 t = tic();
 
@@ -20,16 +20,22 @@ if ~exist('k', 'var')
     k = 1;
 end
 if ~exist('tstep', 'var')
-    tstep = 0.005;
+    tstep = 0.05;
 end
 if ~exist('deltaM', 'var')
-    deltaM = 0;
+    deltaM = 0.1;
 end
 if ~exist('maxStep', 'var')
-    maxStep = 1e7;
+    maxStep = 1e9;
 end
 if ~exist('beta', 'var')
     beta = 1;
+end
+if ~exist('nDataPoints', 'var')
+    nDataPoints = 10;
+end
+if ~exist('save', 'var')
+    save = true;
 end
 
 kB = 1.380649e-23;
@@ -38,16 +44,23 @@ kB = 1.380649e-23;
 x = zeros(N + 2, 1);
 v = zeros(N + 2, 1);
 a = zeros(N + 2, 1);
-v(2) = Tl;
-v(N+1) = Tr;
-xil0 = 2.*lambda.*kB.*Tl;
-xir0 = 2.*lambda.*kB.*Tr;
 
-%array of masses
+% chi terms
+xil0 = sqrt(2.*lambda.*kB.*Tl);
+xir0 = sqrt(2.*lambda.*kB.*Tr);
+
+% array of masses
 m = 1 - deltaM + (2*deltaM)*rand(N + 2, 1);
 m(1) = inf;
 m(N+2) = inf;
 
+% initial conditions
+%v(2) = sqrt(kB*Tl/m(2));
+%v(N+1) = sqrt(kB*Tr/m(N+2));
+v(2) = Tl;
+v(N+1) = Tr;
+
+% pre-allocation
 newa = zeros(N+2,1);
 xl = zeros(N+2,1);
 xr = zeros(N+2,1);
@@ -58,50 +71,64 @@ cuber = zeros(N+2,1);
 
 fcum = zeros(N+2, 1);
 
-for step = 1:1000
+j = zeros(nDataPoints,1);
+T = zeros(nDataPoints,1);
 
-    x = x + v.*tstep + 0.5.*a.*tstep.^2;
+for i = 1:nDataPoints
 
-    xl = circshift(x, 1);
-    xl(N+2) = 0;
-
-    cubel = (xl - x);
-    cubel = cubel.*cubel.*cubel;
-    cuber = -circshift(cubel,-1);
-    cuber(N+1) = (x(N+1) - x(N))^3;
-    cuber(1) = 0;
-
-    xr = circshift(x,-1);
-    xr(1) = 0;
-
-%     x = gpuArray(x);
-%     xl = gpuArray(xl);
-%     xr = gpuArray(xr);
-%     fl = gpuArray(fl);
-%     cubel = gpuArray(cubel);
-%     cuber = gpuArray(cuber);
-%     v = gpuArray(v);
- 
-    fl = k.*xl + beta.*cubel;
-    fstep = fl + k.*(- 2.*x + xr) + beta.*cuber;
-
-    xiL = xil0*randn;
-    xiR = xir0*randn;
-
-    fstep(2) = fstep(2) + xiL - lambda.*v(2);
-    fstep(N+1) = fstep(N+1) + xiR - lambda.*v(N+1);
-
-    newa = fstep./m;
-
-
-    % calculating v(t + delta_t)
-    v = v + 0.5.*(a + newa).*tstep;
-
-    a = newa;
-    fcum = fcum + fl.*v;
-
+    for step = 1:maxStep/nDataPoints
+    
+        x = x + v.*tstep + 0.5.*a.*tstep.^2;
+    
+        xl = circshift(x, 1);
+        xl(N+2) = 0;
+    
+        cubel = (xl - x);
+        cubel = cubel.*cubel.*cubel;
+        cuber = -circshift(cubel,-1);
+        cuber(N+1) = (x(N+1) - x(N))^3;
+        cuber(1) = 0;
+    
+        xr = circshift(x,-1);
+        xr(1) = 0;
+    
+    %     x = gpuArray(x);
+    %     xl = gpuArray(xl);
+    %     xr = gpuArray(xr);
+    %     fl = gpuArray(fl);
+    %     cubel = gpuArray(cubel);
+    %     cuber = gpuArray(cuber);
+    %     v = gpuArray(v);
+     
+        fl = k.*xl + beta.*cubel;
+        fstep = fl + k.*(- 2.*x + xr) + beta.*cuber;
+    
+        xiL = xil0*randn;
+        xiR = xir0*randn;
+    
+        fstep(2) = fstep(2) + xiL - lambda.*v(2);
+        fstep(N+1) = fstep(N+1) + xiR - lambda.*v(N+1);
+    
+        newa = fstep./m;
     
     
+        % calculating v(t + delta_t)
+        v = v + 0.5.*(a + newa).*tstep;
+    
+        a = newa;
+        fcum = fcum + fl.*v;
+    
+        
+        
+    end
+
+    m(1) = 0;
+    m(N+2) = 0;
+    j(i) = sum(fcum)./(maxStep.*(N-1));
+    T(i) = sum(m.*v.*v)./(kB*N);
+    m(1) = Inf;
+    m(N+2) = Inf;
+
 end
 
 %disp(fstep);
@@ -109,11 +136,43 @@ end
 %disp(j);
 %disp(sum(fl)/N);
 
-j = sum(fcum)/(maxStep.*(N-1));
 time = toc(t);
 dat = [x,v,a];
 
+nIterations = linspace(0, maxStep, nDataPoints);
+
+%{
+disp(T)
+disp(m)
+disp(v)
+disp(kB)
+disp(N)
+%}
+
+plotTitle = "{\Delta}m = " + deltaM + ", {\beta} = " + beta + ", {N} = " + N...
+    + ", {T_l} = " + Tl + ", {T_r} = " + Tr;
+
+plotT = figure();
+plot(nIterations, T);
+xlabel('Number of Iterations');
+ylabel('Temperature');
+title(plotTitle);
+
+plotJ = figure();
+plot(nIterations, j);
+xlabel('Number of Iterations');
+ylabel('Heat Current {j}');
+title(plotTitle);
+
+if save
+    saveas(plotT, "figures/temperatureE" + log10(maxStep) + "dm" + deltaM + "v0=T-2"+ ".png");
+    saveas(plotJ, "figures/heatcurrentE" + log10(maxStep) + "dm" + deltaM + "v0=T-2"+".png");
 end
+
+
+end
+
+
 
 
 
